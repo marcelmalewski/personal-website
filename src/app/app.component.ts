7a116ed9084b46315996ea8d2cb0fbaf6b35ca83
Marcel Malewski <mmalewski9@gmail.com>
2026-01-07 21:54:03 +0100

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports: [NgOptimizedImage],
 })
 export class AppComponent {}
